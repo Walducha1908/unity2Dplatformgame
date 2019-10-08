3383087f07c287f7d1093629e5090a04837fff30
Walducha1908 <walduchampbw1908@gmail.com>
2019-10-08 17:48:16 +0200

    character changed, jump hacking is not possible now

--- MoblikiAndroid2D/Assets/Scripts/PlayerControls.cs
+++ MoblikiAndroid2D/Assets/Scripts/PlayerControls.cs
@@ -6,25 +6,50 @@ public class PlayerControls : MonoBehaviour
 {
     public GameObject player;
     public Rigidbody2D rb;
+    public Camera mainCamera;
     public float jumpPower;
+
+    public bool hidden;
+    private int i = 0;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();        
+        rb = GetComponent<Rigidbody2D>();
+        hidden = false;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if (player.transform.position.y < -7.48f)
+            if (player.transform.position.y < -7.67f)
+            {
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            }
         }
 
         foreach (Touch touch in Input.touches)
         {
-            if (player.transform.position.y < -7.48f)
+            if (player.transform.position.y < -7.67f)
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+
+        if (Swiping.swipingDown)
+        {
+            player.transform.localScale /= 2f;
+            hidden = true;
+        }
+
+        if (hidden)
+        {
+            i++;
+        }
+
+        if (i > 50)
+        {
+            i = 0;
+            player.transform.localScale *= 2f;
+            hidden = false;
         }
     }
 }
---
+++ MoblikiAndroid2D/Assets/Scripts/cameraMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public GameObject player;
+
+    private Camera myCamera;
+    private Vector3 lastPlayerPosition;
+
+    void Start()
+    {
+        myCamera = this.GetComponent<Camera>();
+        lastPlayerPosition = player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+
+        Vector3 move = (lastPlayerPosition - player.transform.position);
+        Debug.Log(move);
+        myCamera.transform.position = myCamera.transform.position - move;
+        lastPlayerPosition = player.transform.position;
+    }
+}

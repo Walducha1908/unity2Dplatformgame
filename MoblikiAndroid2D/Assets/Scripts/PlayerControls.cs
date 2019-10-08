67f8c14ad50706220a96cdfb9036bf012bac72a1
Walducha1908 <walduchampwb1908@gmail.com>
2019-10-08 14:06:21 +0200

    background and character added, jumping is possible too

---
+++ MoblikiAndroid2D/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D rb;
+    public float jumpPower;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (player.transform.position.y < -7.48f)
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (player.transform.position.y < -7.48f)
+                rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+}
---
+++ MoblikiAndroid2D/Assets/Scripts/Swiping.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swiping : MonoBehaviour
+{
+    public static bool swipingDown;
+    public static bool swipingUp;
+    public static bool swipingLeft;
+    public static bool swipingRight;
+
+    private bool swiping = false;
+    private bool eventSent = false;
+    private Vector2 lastPosition;
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        if (Input.GetTouch(0).deltaPosition.sqrMagnitude != 0)
+        {
+            if (swiping == false)
+            {
+                swiping = true;
+                lastPosition = Input.GetTouch(0).position;
+                return;
+            }
+            else
+            {
+                if (!eventSent)
+                {
+                    Vector2 direction = Input.GetTouch(0).position - lastPosition;
+
+                    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                    {
+                        if (direction.x > 0)
+                        {
+                            swipingRight = true;
+                            swipingLeft = false;
+                        }
+                        else
+                        {
+                            swipingLeft = true;
+                            swipingRight = false;
+                        }
+                        swipingDown = false;
+                        swipingUp = false;
+                    }
+                    else
+                    {
+                        if (direction.y > 0)
+                        {
+                            swipingUp = true;
+                            swipingDown = false;
+                        }
+                        else
+                        {
+                            swipingDown = true;
+                            swipingUp = false;
+                        }
+                        swipingLeft = false;
+                        swipingRight = false;
+                    }
+
+                    eventSent = true;
+                }
+            }
+        }
+        else
+        {
+            swiping = false;
+            eventSent = false;
+        }
+    }
+}

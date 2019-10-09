d07397b07675184ed9487952ce3a4967eef94bd7
Walducha1908 <walduchampwb1908@gmail.com>
2019-10-09 23:20:30 +0200

    added first level gui

---
+++ MoblikiAndroid2D/Assets/Scripts/firstLevelStart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class firstLevelStart : MonoBehaviour
+{
+    public static bool mathsBonus = false;
+    public static bool infBonus = false;
+    public static bool physicsBonus = false;
+
+    public void mathsClicked()
+    {
+        mathsBonus = true;
+        whateverClicked();
+    }
+
+    public void physicsClicked()
+    {
+        physicsBonus = true;
+        whateverClicked();
+    }
+
+    public void infClicked()
+    {
+        infBonus = true;
+        whateverClicked();
+    }
+
+    public void whateverClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+}

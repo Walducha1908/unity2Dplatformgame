5595a6ff702e0aac9d2df53d4fb59f2351d8322b
KonradK98 <zangetsuxxl2@gmail.com>
2020-01-11 14:48:44 +0100

    parralax effect on new background

--- MoblikiAndroid2D/Assets/Scripts/MoveScripts/cameraMove.cs
+++ MoblikiAndroid2D/Assets/Scripts/MoveScripts/cameraMove.cs
@@ -6,6 +6,10 @@ public class cameraMove : MonoBehaviour
 {
     public GameObject player;
     public GameObject bg;
+    public GameObject mountains;
+    public GameObject background;
+    public float parralaxMountains;
+    public float parralaxBackground;
 
     private Camera myCamera;
     private float lastPlayerPosition;
@@ -19,6 +23,11 @@ public class cameraMove : MonoBehaviour
     void LateUpdate()
     {
         float move = lastPlayerPosition + 6.1f;
+        float moveMountains = (myCamera.transform.position.x * parralaxMountains);
+        float moveBackground = (myCamera.transform.position.x * parralaxBackground);
+
+        mountains.transform.position = new Vector3(moveMountains, mountains.transform.position.y, mountains.transform.position.z);
+        background.transform.position = new Vector3(moveBackground, background.transform.position.y, background.transform.position.z);
 
         if (myCamera.transform.position.x - bg.transform.position.x < 4.6f)
         {
